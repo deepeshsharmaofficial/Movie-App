ab101cdfa007162326b5cbe1fdce63d9b8152477
Deepesh Sharma <deep92112018@gmail.com>
2023-08-07 22:29:04 +0530

    State in React

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,7 +1,27 @@
 import {Component} from "react";
 
 class MovieCard extends Component {
+
+/*
+    We are actually hardcoding every movie-card.
+    We need to find a way to actually get rid of this Hardcoding.
+    React say "If you want to store any information or data of the component make use of states."
+*/
+
+    constructor() {
+        super();
+        this.state = {
+            title: "The Avengers",
+            plot:"Supernatural powers shown in the movie.",
+            price: 199,
+            rating: 8.9,
+        }
+    }
+
     render() {
+        const {title, plot, price, rating} = this.state; // alternative of this.state.title
+        // After using this, 
+
         return (
             <div className="main">
                 <div className="movie-card">
@@ -10,37 +30,37 @@ class MovieCard extends Component {
                     </div>
 
                     <div className="right">
-                        <div className="title">The Avengers</div>
-                        <div className="plot">Supernatural power shown in the movie.</div>
-                        <div className="price">Rs. 199</div>
+                        <div className="title">{this.state.title}</div>
+                        <div className="plot">{plot}</div>
+                        <div className="price">Rs.{price}</div>
 
                         <div className="footer">
-                            <div className="rating">8.9</div>
+                            <div className="rating">{rating}</div>
                             <div className="star-dis">
                                 <img
                                 src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png" 
                                 alt="decrease" 
-                                className="btn str-btn"/>
+                                className="str-btn"/>
 
                                 <img
                                 src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" 
                                 alt="" 
                                 className="stars"/>
-
+                                
                                 <img
                                 src="https://cdn-icons-png.flaticon.com/128/748/748113.png" 
                                 alt="increase" 
-                                className="btn str-btn"/>
+                                className="str-btn"/>
 
                                 <span>0</span>
 
                             </div>
-                            <button className="btn favourite-btn">Favourite</button>
-                            <button className="btn cart-btn">Add to Card</button>
+                            <button className="favourite-btn">Favourite</button>
+                            <button className="cart-btn">Add to Card</button>
                         </div>
                     </div>
                 </div>
-
+                
 
             </div>
         )
