b02051c59dfdec68a3961a1cd9422de4666b03c6
Deepesh Sharma <deep92112018@gmail.com>
2023-08-10 16:58:12 +0530

    Creating Movies list

---
+++ src/MovieList.js
@@ -0,0 +1,17 @@
+import {Component} from "react";
+import MovieCard from "./Moviecard";
+
+class MovieList extends Component {
+
+    render() {
+
+        return (
+            <>
+            <MovieCard />
+            </>
+        )
+
+    }
+}
+
+export default MovieList;
