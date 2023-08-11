3f1e2ab3f6e5eb135df97770d14adc39252b9d04
Deepesh Sharma <deep92112018@gmail.com>
2023-08-12 01:40:58 +0530

    Passing data through props

--- src/MovieList.js
+++ src/MovieList.js
@@ -2,12 +2,88 @@ import {Component} from "react";
 import MovieCard from "./Moviecard";
 
 class MovieList extends Component {
+    /*
+      We are actually hardcoding every movie-card.
+      We need to find a way to actually get rid of this Hardcoding.
+      React say "If you want to store any information or data of the component make use of states."
+    */
+
+    constructor() {
+        super();
+        this.state = {
+            title: "The Avengers",
+            plot: "Supernatural powers shown in the movie.",
+            price: 199,
+            rating: 8.9,
+            stars: 0,
+            fav: false,
+            cart: false,
+        };
+        // this.addStars = this.addStars.bind(this);
+    }
+
+    addStars = () => {
+        // console.log("this.state: ", this.state);
+        // this.state.stars = this.state.stars + 0.5;
+
+        if (this.state.stars >= 5) {
+            return;
+        }
+
+        // 1st form of setState()
+        // this.setState({
+        //     stars: this.state.stars + 0.5
+        // });
+
+        // 2nd form of setState()
+        // this.setState((prevState) => ({ stars: prevState.stars + 0.5 }));
+        this.setState((prevState) => {
+            return {
+                stars: prevState.stars + 0.5,
+            };
+        });
+    };
+
+    decStars = () => {
+        if (this.state.stars <= 0) {
+            return;
+        }
+
+        this.setState((prevState) => {
+            return {
+                // if (prevState.stars >= 0) {
+                stars: prevState.stars - 0.5,
+                // }
+            };
+        });
+    };
+
+    handleFav = () => {
+        this.setState({
+            fav: !this.state.fav,
+        });
+    };
+
+    handleCart = () => {
+        this.setState({
+            cart: !this.state.cart,
+        });
+    };
 
     render() {
-
+        const { title, plot, price, rating, stars, fav, cart } = this.state;
         return (
             <>
-            <MovieCard />
+                {/* <MovieCard title={title} 
+                        plot={plot} 
+                        price={price}
+                        rating = {rating}
+                        stars = {stars}
+                        fav = {fav}
+                        cart = {cart}/>
+            </> */}
+                {/* We Can directly pass there */}
+                <MovieCard movies={this.state} />
             </>
         )
 
--- src/Moviecard.js
+++ src/Moviecard.js
@@ -2,129 +2,65 @@ import {Component} from "react";
 
 class MovieCard extends Component {
 
-/*
-    We are actually hardcoding every movie-card.
-    We need to find a way to actually get rid of this Hardcoding.
-    React say "If you want to store any information or data of the component make use of states."
-*/
-
-constructor() {
-    super();
-    this.state = {
-        title: "The Avengers",
-        plot:"Supernatural powers shown in the movie.",
-        price: 199,
-        rating: 8.9,
-        stars :0,
-        fav: false,
-        cart: false,
-    }
-    // this.addStars = this.addStars.bind(this);
-}
-
-addStars = () => {
-    // console.log("this.state: ", this.state);
-    // this.state.stars = this.state.stars + 0.5;
-
-
-    if(this.state.stars >= 5) {
-        return;
-    }
-
-    // 1st form of setState()
-    // this.setState({
-    //     stars: this.state.stars + 0.5
-    // });
-
-    // 2nd form of setState()
-    // this.setState((prevState) => ({ stars: prevState.stars + 0.5 }));
-    this.setState((prevState) => {
-        return{
-            stars: prevState.stars + 0.5
-        }
-    });
-}
-
-decStars = () => {
-    if (this.state.stars <= 0) {
-        return;
-    }
-
-    this.setState((prevState) => {
-        return {
-            // if (prevState.stars >= 0) {
-                stars: prevState.stars - 0.5
-            // }
-        }
-    });
-}
-
-handleFav = () => {
-    this.setState({
-        fav: !this.state.fav
-    })
-}
-
-handleCart = () => {
-    this.setState({
-        cart: !this.state.cart
-    })
-}
-
 render() {
-    const {title, plot, price, rating, stars, fav, cart} = this.state;
-    return (
-        <div className="main">
-            <div className="movie-card">
-                <div className="left">
-                    <img alt="poster" src="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"></img>
-                </div>
+    const {title, plot, price, rating, stars, fav, cart} = this.props.movies;
+        
+        // We can do it like that also
+        // const {movies: data} = this.props;
+        // const {title, plot, price, rating, stars, fav, cart} = data;
+        
+        return (
+            <div className="main">
+                <div className="movie-card">
+                    <div className="left">
+                        <img alt="poster" src="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg"></img>
+                    </div>
 
-                <div className="right">
-                    <div className="title">{this.state.title}</div>
-                    <div className="plot">{plot}</div>
-                    <div className="price">Rs.{price}</div>
+                    <div className="right">
+                        <div className="title">{title}</div>
+                        <div className="plot">{plot}</div>
+                        <div className="price">Rs.{price}</div>
 
-                    <div className="footer">
-                        <div className="rating">{rating}</div>
-                        <div className="star-dis">
-                            <img
-                            src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png" 
-                            alt="decrease" 
-                            className="str-btn"
-                            onClick={this.decStars}
-                            />
+                        <div className="footer">
+                            <div className="rating">{rating}</div>
+                            <div className="star-dis">
+                                <img
+                                src="https://cdn-icons-png.flaticon.com/128/2801/2801932.png" 
+                                alt="decrease" 
+                                className="str-btn"
+                                onClick={this.decStars}
+                                />
 
-                            <img
-                            src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" 
-                            alt="" 
-                            className="stars"/>
+                                <img
+                                src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" 
+                                alt="" 
+                                className="stars"/>
+                                
+                                <img
+                                src="https://cdn-icons-png.flaticon.com/128/748/748113.png" 
+                                alt="increase" 
+                                className="str-btn"
+                                onClick={this.addStars}
+                                />
+
+                                <span>{stars}</span>
+
+                            </div>
+
+                            {/* {fav ? <button className="unfavourite-btn" onClick={this.handleFav}>Un-favourite</button> : 
+                            <button className="favourite-btn" onClick={this.handleFav}>Favourite</button>} */}
+
+                            <button className={fav ? "unfavourite-btn":"favourite-btn"} onClick={this.handleFav}>{fav ? "Unfavourite":"Favourite"}</button>
                             
-                            <img
-                            src="https://cdn-icons-png.flaticon.com/128/748/748113.png" 
-                            alt="increase" 
-                            className="str-btn"
-                            onClick={this.addStars}
-                            />
-
-                            <span>{stars}</span>
-
+                            <button className={cart ? "remove-cart-btn" : "cart-btn"} onClick={this.handleCart}>{cart ? "Remove to Card" : "Add to Card"}</button>
                         </div>
-
-                        {/* {fav ? <button className="unfavourite-btn" onClick={this.handleFav}>Un-favourite</button> : 
-                        <button className="favourite-btn" onClick={this.handleFav}>Favourite</button>} */}
-
-                        <button className={fav ? "unfavourite-btn":"favourite-btn"} onClick={this.handleFav}>{fav ? "Unfavourite":"Favourite"}</button>
-                        
-                        <button className={cart ? "remove-cart-btn" : "cart-btn"} onClick={this.handleCart}>{cart ? "Remove to Card" : "Add to Card"}</button>
                     </div>
                 </div>
-            </div>
-            
+                
 
-        </div>
-    )
-}
+            </div>
+        )
+    }
 }
 
 export default MovieCard;
