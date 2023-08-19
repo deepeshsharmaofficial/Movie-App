4a6bff5aa02def69c63c6d7d22d31e912cb4b111
Deepesh Sharma <deep92112018@gmail.com>
2023-08-19 20:16:34 +0530

    inline styles in React

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
 import {Component} from "react";
-
 class Navbar extends Component {
     render() {
         return (
             <>
-            <div>
+            <div style={{ width: "100%",
+                        height: 70,
+                        background: "red",
+                        justifyContent: "space-between"}}>
                 <div>Title</div>
                 <div>
                     <img src="" alt="Cart Icon" />
@@ -15,5 +17,4 @@ class Navbar extends Component {
         )
     }
 }
-
 export default Navbar;
